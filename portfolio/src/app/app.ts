e67a96fd310e8e350c0b38b53cc688c949a6387b
José Pedro <pedrogluna@hotmail.com.br>
2026-09-25 00:08:13 -0300

    Fix: correção no build dos componentes

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { Navbar } from "./pages/navbar/navbar";
-import { Sobre } from "./pages/sobre/sobre";
-import { Projetos } from "./pages/projetos/projetos";
-import { Estudos } from "./pages/estudos/estudos";
+import { Navbar } from './pages/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, Navbar, Sobre, Projetos, Estudos],
+  imports: [RouterOutlet, RouterLink, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class AppComponent {}
